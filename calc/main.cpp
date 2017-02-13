91f5a2af5dacc2638b2633b8179ec2a48437d5c9
Your Name <you@example.com>
2017-02-13 17:35:42 +0100

    first commit

---
+++ calc/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //czolowka + menu
+
+
+    //pobieranie danych
+
+
+    //przetwarzanie danych (dodawanie ...) i wypisywanie wyniku
+
+
+
+
+    return 0;
+}
