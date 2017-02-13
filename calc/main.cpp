32aca4ddbe5534e81e651512c45792e519eb8c48
8D <you@example.com>
2017-02-13 17:58:41 +0100

    commit oliwier

--- calc/main.cpp
+++ calc/main.cpp
@@ -11,7 +11,8 @@ int main()
 
 
     //przetwarzanie danych (dodawanie ...) i wypisywanie wyniku
-
+    cout << "Ich roznica " << lb1 - lb2 << endl;
+    cout << "Ich suma wynosi " << lb1 + lb2 << endl;
 
 
 
