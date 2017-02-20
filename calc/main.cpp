c5fac13c2f55965aa2a5d71a9cc32845855f2302
sala multimedialna 6 <sala multimedialna 6>
2017-02-20 18:53:28 +0100

    zmiany Ani

--- calc/main.cpp
+++ calc/main.cpp
@@ -4,23 +4,14 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
     cout << " WITAJ PRZYBYSZU " << endl;
-    cout << " widze, ze chcesz skorzystac z mego kalkulatora " << endl;
-    cout << " PODAJ CO CHCESZ ZROBIÆ " << endl;
-    char symbol
-    cin >> symbol;
-    if ( symbol == '+' ) {}
-    if ( symbol == '-' ) {}
-    //czolowka + menu
+    cout << " Widze, ze chcesz skorzystac z mego kalkulatora. " << endl;
 
-
-    //pobieranie danych
-    float lb1
-    float lb2
-    cout << "podaj pierwsz¹ liczbê" << endl;
+    float lb1;
+    float lb2;
+    cout << "Podaj pierwsza liczbe" << endl;
     cin  >> lb1;
-    cout << "podaj drug¹ liczbê" << endl;
+    cout << "Podaj druga liczbe" << endl;
     cin  >> lb2;
 
 
@@ -28,7 +19,7 @@ int main()
     cout << "Ich roznica " << lb1 - lb2 << endl;
     cout << "Ich suma wynosi " << lb1 + lb2 << endl;{
 
-    
+
 
     return 0;
-}
+}}
